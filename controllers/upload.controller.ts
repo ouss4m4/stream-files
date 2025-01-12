4476c691ed5927f625982bca6b82266202ac26cb
Oussama <bz.oussama@gmail.com>
2025-01-12 19:08:59 +0400

    abstract file process to single function (use on worker later)

--- controllers/upload.controller.ts
+++ controllers/upload.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { streamCsvFromDisk } from '../services/streamCsvFromDIsk.service';
 import { downloadImages } from '../services/downloadImage.service';
 import { CsvRow, UploadResponse } from '../shared/types';
+import { processCsv } from '../services/processFileUpload.service';
 
 export class UploadController {
   public static async upload(req: Request, res: Response): Promise<void> {
@@ -10,23 +11,7 @@ export class UploadController {
       return;
     }
     const filePath = req.file.path;
-    // TODO: spawn a worker to process the file 1 worker/file
-    const csvStream = streamCsvFromDisk(filePath);
-    let errorsFetching: string[] = [];
-    let success = 0;
-    for await (const row of csvStream) {
-      // dispatch all images in a single row at once
-      delete row['sku'];
-      const rowResult = await downloadImages(Object.values(row));
-      errorsFetching = errorsFetching.concat(rowResult.errors);
-      success += rowResult.success;
-    }
-
-    const result: UploadResponse = {
-      success: true,
-      message: `Successfully fetched ${success} images`,
-      errors: errorsFetching,
-    };
-    res.json(result);
+    // TODO: spawn a worker to process this process
+    const response = await processCsv(filePath, res);
   }
 }
