eccc2d65ad819514d8df7d7cdffbbd73cc166762
Oussama <bz.oussama@gmail.com>
2025-01-12 20:51:00 +0400

    add test for services

--- middleware/fileUpload.middleware.ts
+++ middleware/fileUpload.middleware.ts
@@ -2,7 +2,7 @@ import multer, { diskStorage } from 'multer';
 
 const storage = diskStorage({
   destination: `${process.cwd()}/uploads`,
-  filename: (req, file, cb) => {
+  filename: (_, file, cb) => {
     const filename = `${Date.now()}-${file.originalname}`;
     cb(null, filename);
   },
