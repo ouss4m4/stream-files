8b3c2f218b4e7fa73ed022a28200eabfa11061d7
Oussama <oussama@notifyai.io>
2025-01-13 11:11:29 +0400

    add cluster support

--- index.ts
+++ index.ts
@@ -1,3 +1,20 @@
 import { startServer } from './app';
+import cluster from 'node:cluster';
+import { availableParallelism } from 'node:os';
+import process from 'node:process';
 
-startServer();
+// console.log(cpus().length);
+const numCPUs = availableParallelism();
+
+if (cluster.isPrimary) {
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  startServer();
+}
